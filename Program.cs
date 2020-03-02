6353f5bd3041f9d81ba3b23a6aa7632e3f781817
James Naylor <jamesnaylor85@gmail.com>
2020-03-02 22:23:12 +0000

    Migrate to Blazor 3.2.0 ✨

--- Program.cs
+++ Program.cs
@@ -1,16 +1,44 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Blazor.Extensions.Logging;
+using Microsoft.Extensions.Logging;
+using Blazor.Extensions.Storage;
+using Microsoft.Extensions.Caching.Memory;
+using ScryfallApi.Client;
+using System.Net.Http;
 
 namespace deckdiff
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            // Disabled until https://github.com/BlazorExtensions/Logging/issues/41
+            // builder.Services.AddLogging(builder => builder
+            //        .AddBrowserConsole()
+            //        .SetMinimumLevel(LogLevel.Trace)
+            //    );
+
+
+            builder.Services.AddStorage();
+
+            builder.Services.AddScoped<IMemoryCache>(service => new MemoryCache(new MemoryCacheOptions()));
+
+            builder.Services.AddSingleton<ScryfallApiClient>(service => {
+                var logger = service.GetRequiredService<ILogger<ScryfallApiClient>>();
+                var cache = service.GetRequiredService<IMemoryCache>();
+                var httpClient = new HttpClient();
+                httpClient.BaseAddress = new Uri("https://api.scryfall.com/");
+                return new ScryfallApiClient(httpClient, logger, cache);
+            });
+
+            await builder.Build().RunAsync();
         }
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
     }
 }
